f83f4adef19d4ea66b0006d1821b606f1f10aee3
Haytham Al-Dokanji <haldokanji@bloomberg.net>
2017-09-22 17:25:06 -0400

    First  push

--- src/api/graphql/schema.js
+++ src/api/graphql/schema.js
@@ -20,4 +20,10 @@ export default new GraphQLSchema({
     }),
   }),
   // mutation: ...
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    fields: () => ({
+      // ...ArticleMutations,
+    }),
+  }),
 });
